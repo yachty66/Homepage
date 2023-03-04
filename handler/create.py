f6e9abbbe89723b69043465673b2611aa9a822e5
Max Hager <maxhager28@gmail.com>
2023-03-04 13:43:39 +0100

    s

--- handler/create.py
+++ handler/create.py
@@ -14,7 +14,7 @@ credentials_dict = {
     'client_id': os.environ['BACKUP_CLIENT_ID'],
     'client_email': os.environ['BACKUP_CLIENT_EMAIL'],
     'private_key_id': os.environ['BACKUP_PRIVATE_KEY_ID'],
-    'private_key': os.environ['BACKUP_PRIVATE_KEY'],
+    'private_key': os.environ['BACKUP_PRIVATE_KEY'].replace('\\n', '\n'),
 }
 
 credentials = ServiceAccountCredentials.from_json_keyfile_dict(
