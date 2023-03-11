35d56d8817460b306eae87fd6ded66f0c69fd861
Max Hager <maxhager28@gmail.com>
2023-03-11 10:26:03 -0800

    s

---
+++ homepage_react/src/About.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import ReactMarkdown from "react-markdown";
+import about from "./markdown_about/about.md";
+import remarkMath from "remark-math";
+import Container from "react-bootstrap/Container";
+import "./css/About.css";
+import { BlockMath, InlineMath } from "react-katex";
+import rehypeRaw from 'rehype-raw';
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { terms: null };
+  }
+
+  componentWillMount() {
+    fetch(about)
+      .then((response) => response.text())
+      .then((text) => {
+        this.setState({ about: text });
+      });
+  }
+
+  render() {
+    return (
+      <Container className="about-container mt-5">
+        <ReactMarkdown
+          className="markdown"
+          children={this.state.about}
+          rehypePlugins={[rehypeRaw]}
+          remarkPlugins={[remarkMath]}
+          components={{
+            math: ({ value }) => <BlockMath math={value} />,
+            inlineMath: ({ value }) => <InlineMath math={value} />,
+          }}
+        />
+      </Container>
+    );
+  }
+}
+
+export default About;
---
+++ homepage_react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "./Header";
+import About from "./About";
+import './css/styles.css';
+
+function App () {
+  return (
+    <div className="App">
+      <About />
+    </div>
+  );
+};
+
+export default App;
